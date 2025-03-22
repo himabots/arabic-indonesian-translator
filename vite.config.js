604179258261f38ddd7aed5ff8e047484415f434
himabots <hnugroho@gmail.com>
2025-03-22 11:30:12 +0400

    Update vite.config.js to properly bundle hark dependency

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['hark'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/hark/, /node_modules/],
+    },
+    rollupOptions: {
+      external: [],
+    },
+  },
 })
